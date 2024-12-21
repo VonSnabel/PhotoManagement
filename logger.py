685b0077180300bb54c0dd9d5ffe87303daca17f
Server <danegodave@gmail.com>
2024-12-21 22:54:34 +0100

    fixed problem with relative and absolut path of log file

--- logger.py
+++ logger.py
@@ -1,12 +1,13 @@
 import logging
+import os
 
 def setupLogger():
     logger = logging.getLogger("PhotoLogger")
     if not logger.hasHandlers():
         logger.setLevel(logging.DEBUG)
-
-
-        fileHandler = logging.FileHandler("../logs/photoLog.txt")
+        scriptDir = os.path.dirname(os.path.abspath(__file__))
+        logFilePath = os.path.join(scriptDir, "../logs/photoLog.txt")
+        fileHandler = logging.FileHandler(logFilePath)
         fileHandler.setLevel(logging.DEBUG)
 
 
